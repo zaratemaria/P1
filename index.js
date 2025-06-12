47f1b643ab9b53956f5d5c6efd1e972c80faa76b
Zarate Maria <zarate.mariadelosangeles@iesconcepcion.edu.ar>
2025-06-12 19:44:34 -0300

    nuevo cambio en el codigo

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-let input = document.createElement("input");
-input.type = "text";
-input.placeholder = "Escribí tu nombre";
-
-let titulo = document.getElementById("titulo");
-document.body.insertBefore(input, titulo);
-
-input.addEventListener("input", function () {
-  titulo.textContent = input.value;
-  console.log(input.value);
+document.getElementById("miInput").addEventListener("input", function() {
+  document.getElementById("miH1").textContent = this.value;
 });
+
 
